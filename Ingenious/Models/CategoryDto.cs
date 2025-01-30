af163e558a04ea7e1f83452c7730b0d6b89c43b1
Waqas Ahmad <waqas.a@bingeabletech.com>
2025-01-30 12:15:46 +0500

    add totalProducts column in get categories API

--- Ingenious/Models/CategoryDto.cs
+++ Ingenious/Models/CategoryDto.cs
@@ -10,6 +10,7 @@
         public bool IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+        public int TotalProducts { get; set; }
     }
     public class UpsertCategoryDto
     {
